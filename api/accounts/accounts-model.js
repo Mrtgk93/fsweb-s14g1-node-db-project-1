a9a749fd32ade60b6267d62235ed04d7ebc90e5e
Mrtgk93 <mrtgk93@hotmail.com>
2023-03-27 22:39:55 +0300

    ödev tamamlandı

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,37 @@
+const db = require("../../data/db-config");
 const getAll = () => {
+  // db.select("*").from("accounts");
+  return db("accounts");
   // KODLAR BURAYA
-}
+};
 
-const getById = id => {
+const getById = (id) => {
+  return db("accounts")
+    .where({ id: Number(id) })
+    .first();
   // KODLAR BURAYA
-}
+};
 
-const create = account => {
-  // KODLAR BURAYA
-}
+const getByName = (name) => {
+  return db("accounts").where("name", name).first();
+};
 
-const updateById = (id, account) => {
+const create = async (account) => {
+  const id = await db("accounts").insert(account);
+  return await getById(id);
   // KODLAR BURAYA
-}
+};
 
-const deleteById = id => {
+const updateById = async (id, account) => {
+  await db("accounts").where("id", Number(id)).update(account);
+  return await getById(id);
   // KODLAR BURAYA
-}
+};
+
+const deleteById = (id) => {
+  return db("accounts").where("id", id).del();
+  // KODLAR BURAYA
+};
 
 module.exports = {
   getAll,
@@ -24,4 +39,5 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+  getByName,
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,83 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const {
+  checkAccountId,
+  checkAccountNameUnique,
+  checkAccountPayload,
+} = require("./accounts-middleware");
+const accountsModel = require("./accounts-model");
 
-router.get('/', (req, res, next) => {
+router.get("/", async (req, res, next) => {
   // KODLAR BURAYA
-})
+  try {
+    let accounts = await accountsModel.getAll();
+    res.json(accounts);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/:id', (req, res, next) => {
-  // KODLAR BURAYA
-})
-
-router.post('/', (req, res, next) => {
-  // KODLAR BURAYA
-})
-
-router.put('/:id', (req, res, next) => {
+router.get("/:id", checkAccountId, async (req, res, next) => {
+  try {
+    res.json(req.currentAccount);
+  } catch (error) {
+    next(error);
+  }
   // KODLAR BURAYA
 });
 
-router.delete('/:id', (req, res, next) => {
-  // KODLAR BURAYA
-})
+router.post(
+  "/",
+  checkAccountPayload,
+  checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      let { name, budget } = req.body;
+      let created = await accountsModel.create({ name: name, budget: budget });
+      res.status(201).json(created);
+    } catch (error) {
+      next(error);
+    }
+    // KODLAR BURAYA
+  }
+);
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.put(
+  "/:id",
+  checkAccountId,
+  checkAccountNameUnique,
+  checkAccountPayload,
+  async (req, res, next) => {
+    try {
+      let { name, budget } = req.body;
+      let updatedAccount = await accountsModel.updateById(req.params.id, {
+        name: name,
+        budget: budget,
+      });
+      res.status(200).json(updatedAccount);
+    } catch (error) {
+      next(error);
+    }
+    // KODLAR BURAYA
+  }
+);
+
+router.delete("/:id", checkAccountId, async (req, res, next) => {
   // KODLAR BURAYA
-})
+  try {
+    await accountsModel.deleteById(req.params.id);
+    res.json({ message: `${req.params.id}ID'li kayıt silindi` });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  // KODLAR BURAYA
+  res.status(err.status || 400).json({
+    message: err.message,
+    customMessage: "Hata oluştu",
+  });
+});
 
 module.exports = router;
